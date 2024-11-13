7b118651a73c66001e2c22cc496b8ef03b234e92
ed-roh <edwardroh89@gmail.com>
2024-11-12 23:31:03 -0800

    limit

--- server/src/index.ts
+++ server/src/index.ts
@@ -30,11 +30,11 @@ export const clerkClient = createClerkClient({
 });
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 app.use(clerkMiddleware());
